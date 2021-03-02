c3ba8d3278324f22223710fc53d722b29f53bb49
Mohamed-Abdelhamed <43310877+Mohamed-Abdelhamed@users.noreply.github.com>
2021-03-02 17:35:14 +0200

    check internet events page

--- lib/providers/event_provider.dart
+++ lib/providers/event_provider.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:flutter/widgets.dart';
 import 'package:smallorgsys/models/event.dart';
 import 'package:http/http.dart' as http;
@@ -5,6 +7,7 @@ import 'dart:convert';
 
 class EventsController with ChangeNotifier {
   List<Event> _eventsList = [];
+  String status = '';
 
   void addEvents() async {
     // final response = await http
@@ -128,6 +131,11 @@ class EventsController with ChangeNotifier {
 
   Future<void> fetchAndSetEvents() async {
     try {
+      final result = await InternetAddress.lookup('google.com');
+      if (result.isNotEmpty && result[0].rawAddress.isNotEmpty) {
+        status = 'provider connected';
+        //print(status);
+    try {
       final response = await http
           .get('https://tedxmiu-11c76-default-rtdb.firebaseio.com/events.json');
       final dbData = jsonDecode(response.body) as Map<String, dynamic>;
@@ -153,4 +161,10 @@ class EventsController with ChangeNotifier {
       throw (e);
     }
   }
+  } on SocketException catch (_) {
+      status = 'provider not connected';
+      //print(status);
+      return status;
+    }
+}
 }
