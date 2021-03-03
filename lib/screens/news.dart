3711eea1451ee9120f61b152dbd20ba5afd13c68
Ahmed El-Agroudy <ahmed.3mad.eldin@gmail.com>
2021-03-03 13:53:49 +0200

    Applied FutureBuilder to news page

--- lib/screens/news.dart
+++ lib/screens/news.dart
@@ -5,6 +5,7 @@ import 'package:smallorgsys/providers/news_provider.dart';
 import 'package:smallorgsys/screens/drawer.dart';
 import 'package:smallorgsys/screens/news_details.dart';
 import 'package:cached_network_image/cached_network_image.dart';
+import 'package:http/http.dart' as http;
 
 class NewsPage extends StatefulWidget {
   @override
@@ -13,24 +14,14 @@ class NewsPage extends StatefulWidget {
 
 class _NewsPageState extends State<NewsPage> {
   // var n = News(title: "1", imagePath: ".com", description: "asd")..news;
-  var _isLoading = true;
   var providerNewsController;
-  var _isConnected = true;
 
   @override
   void initState() {
     Provider.of<NewsController>(context, listen: false)
         .fetchAndSetNews()
         .then((_) {
-      setState(() {
-        if (Provider.of<NewsController>(context, listen: false).status ==
-            "provider not connected") {
-          _isConnected = false;
-        } else {
-          _isConnected = true;
-        }
-        _isLoading = false;
-      });
+      setState(() {});
     });
     super.initState();
   }
@@ -39,15 +30,7 @@ class _NewsPageState extends State<NewsPage> {
     Provider.of<NewsController>(context, listen: false)
         .fetchAndSetNews()
         .then((_) {
-      setState(() {
-        if (Provider.of<NewsController>(context, listen: false).status ==
-            "provider not connected") {
-          _isConnected = false;
-        } else {
-          _isConnected = true;
-        }
-        _isLoading = false;
-      });
+      setState(() {});
     });
   }
 
@@ -55,14 +38,31 @@ class _NewsPageState extends State<NewsPage> {
   Widget build(BuildContext context) {
     providerNewsController = Provider.of<NewsController>(context);
     return Scaffold(
-      drawer: DrawerPage(),
-      appBar: AppBar(
-        title: Text('News'),
-      ),
-      body: _isConnected
-          ? _isLoading
-              ? Center(child: CircularProgressIndicator())
-              : RefreshIndicator(
+        drawer: DrawerPage(),
+        appBar: AppBar(
+          title: Text('News'),
+        ),
+        body: FutureBuilder(
+            future: http.get(
+                'https://tedxmiu-11c76-default-rtdb.firebaseio.com/news.json'),
+            builder: (context, snapshot) {
+              if (snapshot.connectionState == ConnectionState.done) {
+                if (snapshot.hasError) {
+                  return Center(
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: <Widget>[
+                        Text('An error ocurred while retriving data,'),
+                        Text('Please check your network connection!'),
+                        RaisedButton(
+                          child: Text("Retry"),
+                          onPressed: () => _refresh(context),
+                        )
+                      ],
+                    ),
+                  );
+                }
+                return RefreshIndicator(
                   onRefresh: () => _refresh(context),
                   child: ListView.builder(
                     itemCount: providerNewsController.news.length,
@@ -73,20 +73,11 @@ class _NewsPageState extends State<NewsPage> {
                           id: providerNewsController.news[index].id);
                     },
                   ),
-                )
-          : Center(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  Text('Please check your network connection!'),
-                  RaisedButton(
-                    child: Text("Retry"),
-                    onPressed: () => _refresh(context),
-                  )
-                ],
-              ),
-            ),
-    );
+                );
+              } else {
+                return Center(child: CircularProgressIndicator());
+              }
+            }));
   }
 
   Widget eventCard({@required imagePath, @required id}) {
